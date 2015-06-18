0cfbf697417cfa261141b6556c8e4369462e9a9f
dcdanko <dcdanko@gmail.com>
2015-06-18 15:02:42 -0400

    reindexer script

---
+++ cmd/neutronium-reindexer/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"github.com/ndaniels/neutronium"
+	"os"
+)
+
+func init() {
+
+	flag.Parse()
+}
+
+func main() {
+	db, err := neutronium.NewReadDB(flag.Arg(0))
+
+	byteOff := int64(0)
+	buf := new(bytes.Buffer)
+	coarsedb := db.CoarseDB
+
+	newFastaIndex, err := os.Open("coarse.fasta.index.new")
+	if err != nil {
+		fatalf("Failed to open new index file: %s\n", err)
+	}
+
+	for i := 0; i < len(coarsedb.Seqs); i++ {
+		buf.Reset()
+		fmt.Fprintf(buf, ">%d\n%s\n", i, string(coarsedb.Seqs[i].Residues))
+
+		err = binary.Write(newFastaIndex, binary.BigEndian, byteOff)
+		if err != nil {
+			fatalf("Failed to write to new index file: %s\n", err)
+		}
+
+		byteOff += int64(buf.Len())
+	}
+
+}
+
+func fatalf(format string, v ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, v...)
+	os.Exit(1)
+}
